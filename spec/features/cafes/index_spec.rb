db7f1e3595b0c03903bee2a92a12d3061b6a40a6
Blake Sergesketter <blakesergesketter@gmail.com>
2023-08-26 16:30:13 -0500

    create a parent index spec, html, routes, and controller. All tests passing.

---
+++ spec/features/cafes/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+# User Story 1, Parent Index 
+
+# For each cafe record
+# As a visitor
+# When I visit '/cafes'
+# Then I see the name of each cafe record in the system
+
+RSpec.describe "Cafes index page", type: :feature do 
+  before :each do 
+    @cafe1 = Cafe.create!(name: "Daily Grind", city: "Evansville", rating: 5, rewards_program: true)
+    @cafe2 = Cafe.create!(name: "Starbucks", city: "Owensboro", rating: 3, rewards_program: true)
+    @cafe3 = Cafe.create!(name: "Bobs Diner", city: "Indianapolis", rating: 4, rewards_program: false)
+  end
+  describe "As a visitor to /cafes" do 
+    it "Shows the names of every cafe record" do 
+      visit '/cafes' 
+
+      expect(page).to have_content("Daily Grind")
+      expect(page).to have_content("Starbucks")
+      expect(page).to have_content("Bobs Diner")
+      expect(page).to have_content(@cafe1.name)
+    end
+  end
+end
